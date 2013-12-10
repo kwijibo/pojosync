6b95a0c5bd8a3e9f6c3357b069a2d66c24b094ed
kwijibo <github@keithalexander.co.uk>
2013-12-10 16:29:19 +0000

    changed package.json and added utils to index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   Client : require('./lib/client.js'),
   Server : require('./lib/server.js'),
-  Store  : require('./lib/store.js')
+  Store  : require('./lib/store.js'),
+  Utils  : require('./lib/utils.js') 
 };
