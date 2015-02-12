fae1b2d08f7d17bd32a11dc732d796b6cbf75ff2
kwijibo <github@keithalexander.co.uk>
2015-02-12 01:28:54 +0000

    fixed bug with objects in arrays not being recursively changesetted

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ module.exports = {
   Server : require('./lib/server.js'),
   Store  : require('./lib/store.js'),
   ServerStore  : require('./lib/serverstore.js'),
-  Utils  : require('./lib/utils.js') 
+  Utils  : require('./lib/utils.js') ,
+  makeTestClient : function(){
+    var jsonify = function(ev, a){ return JSON.stringify(a)};
+    var fun = function(){ return { on: fun, emit: jsonify } };
+    return new this.Client(fun()); 
+  }
 };
